4d59111ec5c2ebcc61ee9654e2a2702792524a53
saivivek2420030018 <2420030018@klh.edu.in>
2025-10-22 21:10:00 +0530

    Create simple-interest.sh

---
+++ simple-interest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# simple-interest.sh - Calculate Simple Interest
+
+echo "Enter Principal:"
+read p
+echo "Enter Rate of Interest:"
+read r
+echo "Enter Time (in years):"
+read t
+
+si=$(( (p * r * t) / 100 ))
+
+echo "Simple Interest = $si"
+chmod +x simple-interest.sh
+./simple-interest.sh
